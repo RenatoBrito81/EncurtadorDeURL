9a473e564eed5bc67d24a6e931358a5731f778d2
Renato Brito <rb.renatobrito@gmail.com>
2020-10-15 05:21:54 -0300

    Inclusão da configuração o typescript e do tslint.

---
+++ src/Controller/URLController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+export class URLController {
+    public async EncurtarURL(req: Request, res: Response): Promise<void>{
+        //Ver se a URL já existe
+
+        //Criar o hash para encurtar a URL
+
+        //Salvar a URL no banco de dados
+
+        //Retornar a URL encurtada
+    }
+}
